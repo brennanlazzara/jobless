14277a535d3a7dc90499c2e25a50ae4c33ba34d1
Tabby Garcia <tabby.garcia1@gmail.com>
2020-05-06 18:23:20 -0500

    added container for home page,inserted main header and bottom section page.created a pages file in components to add our different pages.

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Main from '../components/Main';
+import Bottom from '../components/Bottom';
+import Container from '../components/Container'
 
  const Home = ()=> {
     return (
-        <>
+        <Container>
         <Main />
-        </>
+        <Bottom />
+       </Container>
     )
 
 }
