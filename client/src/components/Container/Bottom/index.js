d53d1b489798b349a490ca44bd627300c753d58e
Brennan Predmore <brennnann@gmail.com>
2020-05-25 14:14:16 -0500

    bottom main div button init w/ ('/joblistings') route

--- client/src/components/Container/Bottom/index.js
+++ client/src/components/Container/Bottom/index.js
@@ -13,7 +13,7 @@ const Bottom = (props) => {
           <CardBody>
            
             <CardText> The world is a very strange place right now. COVID-19 is affecting  people in many ways across the globe. The United Staes has over 30 million Americans that have filed for unemployment benefits within the past three months, and that number continues to rise everyday. Here at Jobless, we want to help those in need of employment due to COVID-19. We have set up a search for you to find remote and essential employment in to help alleviate unemployment rates during these hard times. Jobless is committed to and passionate about helping finding your next job or career.</CardText>
-            <Button>Find Jobs</Button>
+            <Button href={'/joblistings'}>Find Jobs</Button>
           </CardBody>
         </Card>
       </Col>
