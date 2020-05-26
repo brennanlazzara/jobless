28537f95c1f8fa02afb5d1a7bc09e038a2d0a48a
Brennan Predmore <brennnann@gmail.com>
2020-05-26 16:30:47 -0500

    Home and footer on joblistings page

--- client/src/Pages/JobListings/index.js
+++ client/src/Pages/JobListings/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import JobListing from '../../components/JobListingComponents/ListingDisplay'
+import JobListing from '../../components/JobListingComponents/ListingDisplay';
+import Header from '../../components/Header/index';
+import Footer from '../../components/Footer';
 
 const JobListingPage = () => {
   
     return (
       <div className="App">
+        <Header />
         <JobListing />
+        <Footer />
       </div>
     );
   
