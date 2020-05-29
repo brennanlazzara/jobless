ed75fe3af482a7c5ad730f5ec3d92c6cb5363054
Brennan Predmore <brennnann@gmail.com>
2020-05-29 12:14:52 -0500

    header logo path adjust for heroku

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -28,7 +28,7 @@ const Header = ({ auth, logOutUser }) => {
     <div className='header'>
       <Navbar expand='md' style={{ top:-20 }}>
         <NavbarBrand href='/' >
-          <img src='assets/headerlogo.png' alt='logo'></img>
+          <img src='client/public/assets/headerlogo.png' alt='logo'></img>
         </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
