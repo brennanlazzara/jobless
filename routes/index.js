8b7d3ddbfdabaf2f1ab4c3e53ec97b5d32693b31
Brennan Predmore <brennnann@gmail.com>
2020-05-28 20:56:55 -0500

    heroku is the worst ever

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,14 @@ const authRoutes = require('./auth')
 router.use('/api', apiRoutes);
 router.use('/auth', authRoutes);
 
-
 router.use('/api', (req, res) => res.status(404).json('No route for this path'));
 router.use('/auth', (req, res) => res.status(404).json('No route for this path'));
+
+
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+
 
 module.exports = router;
