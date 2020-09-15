ff47504bbdee35732ffa56858953da1f717440c7
Brennan Predmore <brennnann@gmail.com>
2020-09-15 09:49:38 -0500

    client folder replace

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,2 +1,2 @@
-// export const FACEBOOK_AUTH_LINK = 'https://localhost:8000/auth/facebook';
+// export const FACEBOOK_AUTH_LINK = 'https://localhost:8000/auth/facebook'; savin client folder
 export const GOOGLE_AUTH_LINK = process.env.REACT_APP_LOGIN_URL || 'http://localhost:8000/auth/google';
