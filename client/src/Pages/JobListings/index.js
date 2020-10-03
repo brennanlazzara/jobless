c3006c3a9fe6492739f756e731f7cd06e312d03c
Brennan Predmore <brennnann@gmail.com>
2020-10-02 22:00:33 -0500

    refactor /pages/JobListings/index with joblisting

--- client/src/Pages/JobListings/index.js
+++ client/src/Pages/JobListings/index.js
@@ -1,16 +1,51 @@
 import React, { useEffect, useState } from 'react';
-import JobListing from '../../components/JobListingComponents/ListingDisplay';
 import Header from '../../components/Header/index';
 import Footer from '../../components/Footer';
 import axios from 'axios';
-import Search from '../../components/SearchBar/searchBar'
+import Search from '../../components/SearchBar/searchBar';
+import JobTable from '../../components/jobTable/jobTable';
 
 const JobListingPage = () => {
+  // Declare a new state variable to set all employees
   const [jobData, setJobData] = useState([]);
+
+  // Declare a new state to filter
+  const [filterText, setFilterText] = useState('');
+  const filteredJobs = jobData.filter((e) => {
+    // By company
+    if (e.company.toLowerCase().includes(filterText.toLowerCase())) {
+      return true;
+    }
+    // By location
+    else if (e.location.toLowerCase().includes(filterText.toLowerCase())) {
+      return true;
+    }
+    // By title
+    else if (e.title.toLowerCase().includes(filterText.toLowerCase())) {
+      return true;
+    }
+    // By description
+    else if (e.description.toLowerCase().includes(filterText.toLowerCase())) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
   useEffect(() => {
-    axios.get('/api/jobs').then((res) => {
-      setJobData(res.data.data);
-    });
+    // Make a request for a user with a given ID
+    axios
+      .get('/api/jobs')
+      .then(function (response) {
+        // handle success
+        setJobData(response.data.data);
+      })
+      .catch(function (error) {
+        // handle error
+      })
+      .then(function () {
+        // always executed
+      });
   }, []);
 
   return (
@@ -20,11 +55,8 @@ const JobListingPage = () => {
         Your Next Opportunity Awaits...
         <hr />
       </h1>
-      <Search />
-      {jobData.map((job) => {
-        return <JobListing job={job} />;
-      })}
-      ;
+      <Search setFilterText={setFilterText} />
+      <JobTable jobData={filteredJobs} />;
       <Footer />
     </div>
   );
--- client/src/components/jobTable/jobTable.js
+++ client/src/components/jobTable/jobTable.js
@@ -1,86 +1,91 @@
 import React from 'react';
 import { Table } from 'reactstrap';
+// import PropTypes from 'prop-types';
+import Parser from 'html-react-parser';
 
-import './table.css';
+const JobTable = (props) => {
+  {
+    props.jobData.map((e) => {
+      return (
+        <Table>
+          <container style={{ marginLeft: '10px' }}>
+            <thead>
+              <tr>
+                <tr>
+                  <div>
+                    <td>
+                      <h2>
+                        <b>{e.company}</b>
+                      </h2>
+                    </td>
+                  </div>
+                </tr>
 
-const tableComponent = (props) => {
-  JobListing.propTypes = {
-    job: PropTypes.object.isRequired,
-  };
-  return (
-    <Table>
-      <container style={{ marginLeft: '10px' }}>
-        <thead>
-          <tr>
-            <tr>
-              <div>
-                <td>
-                  <h2>
-                    <b>{company}</b>
-                  </h2>
-                </td>
-              </div>
-            </tr>
+                <tr>
+                  <div>
+                    <img
+                      style={{ width: '35px' }}
+                      alt={'companyLogo'}
+                      src={e.company_logo}
+                    ></img>
+                  </div>
+                </tr>
 
-            <tr>
-              <div>
-                <img
-                  style={{ width: '35px' }}
-                  alt={'companyLogo'}
-                  src={company_logo}
-                ></img>
-              </div>
-            </tr>
+                <tr>
+                  <div>
+                    <td>
+                      <h4>{e.location}</h4>
+                    </td>
+                  </div>
+                </tr>
 
-            <tr>
-              <div>
-                <td>
-                  <h4>{location}</h4>
-                </td>
-              </div>
-            </tr>
+                <tr>
+                  <div>
+                    <th style={{ fontWeight: 'bold' }}>Job Title:</th>
+                  </div>
+                  <div>
+                    <td>{e.title}</td>
+                  </div>
+                </tr>
 
-            <tr>
-              <div>
-                <th style={{ fontWeight: 'bold' }}>Job Title:</th>
-              </div>
-              <div>
-                <td>{title}</td>
-              </div>
-            </tr>
+                <tr>
+                  <div>
+                    <th style={{ fontWeight: 'bold' }}>Description:</th>
+                  </div>
+                  <div>
+                    <td> {Parser(e.description)}</td>
+                  </div>
+                </tr>
 
-            <tr>
-              <div>
-                <th style={{ fontWeight: 'bold' }}>Description:</th>
-              </div>
-              <div>
-                <td> {Parser(description)}</td>
-              </div>
-            </tr>
+                <tr>
+                  <div>
+                    <th style={{ fontWeight: 'bold' }}>How To Apply:</th>
+                  </div>
+                  <div>
+                    <td>{Parser(e.how_to_apply)}</td>
+                  </div>
+                </tr>
 
-            <tr>
-              <div>
-                <th style={{ fontWeight: 'bold' }}>How To Apply:</th>
-              </div>
-              <div>
-                <td>{Parser(how_to_apply)}</td>
-              </div>
-            </tr>
-
-            <tr>
-              <div>
-                <th style={{ fontWeight: 'bold' }}>Posted Date:</th>
-              </div>
-              <div>
-                <td>{created_at}</td>
-              </div>
-            </tr>
-          </tr>
-        </thead>
-        <hr />
-      </container>
-    </Table>
-  );
+                <tr>
+                  <div>
+                    <th style={{ fontWeight: 'bold' }}>Posted Date:</th>
+                  </div>
+                  <div>
+                    <td>{e.created_at}</td>
+                  </div>
+                </tr>
+              </tr>
+            </thead>
+            <hr />
+          </container>
+        </Table>
+      );
+    });
+  }
 };
 
-export default tableComponent;
+// JobTable.propTypes = {
+//   job: PropTypes.object.isRequired,
+// };
+
+export default JobTable;
