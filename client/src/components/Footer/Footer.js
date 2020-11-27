3140827e8fe0d6ae832da72850bc14d87a715b9c
Brennan Predmore <brennnann@gmail.com>
2020-11-27 09:43:00 -0600

    footer import startement faGithub/faLinkedin destruction

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './style.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithubAlt, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+// import {  } from '@fortawesome/free-brands-svg-icons';
 
 function index() {
   return (
